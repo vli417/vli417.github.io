2fab94ab12fba12afd1917bafe690cb4aa82adbd
vli417 <iml3676@nyu.edu>
2022-04-06 15:00:08 -0400

    tmim

---
+++ TMIM.js
@@ -0,0 +1,155 @@
+
+
+
+
+
+var img = document.createElement("img");
+img.src = "https://i.pinimg.com/originals/7c/d5/9b/7cd59b1692de13b031d4030c2d5ce50e.jpg";
+img.style.width = "300px";
+img.style.height= "450px";
+var div = document.getElementById("x");
+div.appendChild(img);
+
+img.addEventListener("mouseover", function(){
+    img.style.opacity="0.4";
+
+ });
+ img.addEventListener("mouseout", function(){
+    img.style.opacity="1";
+ });
+
+var img1 = document.createElement("img");
+img1.src = "https://ih1.redbubble.net/image.603688617.1851/poster,504x498,f8f8f8-pad,600x600,f8f8f8.u1.jpg";
+img1.style.width = "400px";
+img1.style.height = "400px";
+var div = document.getElementById("y");
+div.appendChild(img1);
+
+img1.addEventListener("mouseover", function(){
+    img1.style.opacity="0.4";
+
+ });
+ img1.addEventListener("mouseout", function(){
+    img1.style.opacity="1";
+ });
+
+
+var img2 = document.createElement("img");
+img2.src = "https://cdn.cnn.com/cnnnext/dam/assets/140522132732-01-sixties-tv-moments.jpg";
+img2.style.width = "600px";
+img2.style.height = "400px";
+var div = document.getElementById("z");
+div.appendChild(img2);
+
+img2.addEventListener("mouseover", function(){
+    img2.style.opacity="0.4";
+
+ });
+ img2.addEventListener("mouseout", function(){
+    img2.style.opacity="1";
+ });
+
+var img3 = document.createElement("img");
+img3.src = "https://miro.medium.com/max/1400/1*aJSMi2OHKrKpte_corAang.jpeg";
+img3.style.width = "500px";
+img3.style.height= "400px";
+var div = document.getElementById("a");
+div.appendChild(img3);
+
+img3.addEventListener("mouseover", function(){
+    img3.style.opacity="0.4";
+
+ });
+ img3.addEventListener("mouseout", function(){
+    img3.style.opacity="1";
+ });
+
+var img4 = document.createElement("img");
+img4.src = "https://s3.amazonaws.com/libapps/accounts/9225/images/Modern_Screen1940.PNG";
+img4.style.width = "400px";
+img4.style.height= "500px";
+var div = document.getElementById("b");
+div.appendChild(img4);
+
+img4.addEventListener("mouseover", function(){
+    img4.style.opacity="0.4";
+
+ });
+ img4.addEventListener("mouseout", function(){
+    img4.style.opacity="1";
+ });
+
+var img5 = document.createElement("img");
+img5.src = "https://www.washingtonpost.com/graphics/2017/entertainment/tech-generations/images/music_go/ipod.jpg";
+img5.style.width = "400px";
+img5.style.height= "400px";
+var div = document.getElementById("c");
+div.appendChild(img5);
+
+img5.addEventListener("mouseover", function(){
+    img5.style.opacity="0.4";
+
+ });
+ img5.addEventListener("mouseout", function(){
+    img5.style.opacity="1";
+ });
+
+var img6 = document.createElement("img");
+img6.src = "https://i.pinimg.com/originals/67/49/6f/67496f9bda86fc895b2f8a1bc8cb0f5c.jpg";
+img6.style.width = "400px";
+img6.style.height= "400px";
+var div = document.getElementById("d");
+div.appendChild(img6);
+
+img6.addEventListener("mouseover", function(){
+    img6.style.opacity="0.4";
+
+ });
+ img6.addEventListener("mouseout", function(){
+    img6.style.opacity="1";
+ });
+
+var img7 = document.createElement("img");
+img7.src = "http://businessrecord.com/Images/Images/33172.jpg";
+img7.style.width = "400px";
+img7.style.height= "400px";
+var div = document.getElementById("e");
+div.appendChild(img7);
+
+img7.addEventListener("mouseover", function(){
+    img7.style.opacity="0.4";
+
+ });
+ img7.addEventListener("mouseout", function(){
+    img7.style.opacity="1";
+ });
+
+var img8 = document.createElement("img");
+img8.src = "http://businessrecord.com/Images/Images/33172.jpg";
+img8.style.width = "400px";
+img8.style.height= "400px";
+var div = document.getElementById("f");
+div.appendChild(img8);
+
+img8.addEventListener("mouseover", function(){
+    img8.style.opacity="0.4";
+
+ });
+ img8.addEventListener("mouseout", function(){
+    img8.style.opacity="1";
+ });
+
+var img9 = document.createElement("img");
+img9.src = "https://c8.alamy.com/comp/EGJYDW/a-representation-of-the-mind-control-of-the-media-on-people-EGJYDW.jpg";
+img9.style.width = "400px";
+img9.style.height= "400px";
+var div = document.getElementById("g");
+div.appendChild(img9);
+
+img9.addEventListener("mouseover", function(){
+    img9.style.opacity="0.4";
+
+ });
+ img9.addEventListener("mouseout", function(){
+    img9.style.opacity="1";
+ });
